b9e2339962e996f492732af259de9fac7f960824
Rigers Leka <rigers.leka@sisal.al>
2022-01-06 14:32:52 +0100

    initialize logic of pageToShow

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import Home from './Home';
+import Speakers from './Speakers';
 
-const App = () => {
-    return ( 
-        <div>
-            <h2>NextJS Starter</h2>
-        </div>
-    );
-}
+export const ConfigContext = React.createContext();
+
+const pageToShow = (pageName) => {
+  if (pageName === 'Home') return <Home />;
+  if(pageName === 'Speakers') return <Speakers/>
+
+  return <div> Not Found</div>;
+};
+
+const App = ({ pageName }) => {
+
+  return (
+    <div>
+      <h2>NextJS Starter</h2>
+      <ConfigContext.Provider>{pageToShow(pageName)}</ConfigContext.Provider>
+    </div>
+  );
+};
 
 export default App;
