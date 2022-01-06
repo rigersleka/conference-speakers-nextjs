15cd8136a49fafba6c9df293095846e59c544627
Rigers Leka <rigers.leka@sisal.al>
2022-01-06 14:35:17 +0100

    implement logic of ssr for speaker component

---
+++ pages/speakers.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import App from '../src/App';
+import Container from '../src/container';
+
+const Speakers = ({isServer}) => {
+
+  console.log("Speakers" + isServer)
+  return (
+    <Container>
+      <App pageName='Speakers' />
+    </Container>
+  );
+};
+
+Speakers.getInitialProps = async ({ req }) => {
+  const isServer = !!req;
+  if (isServer) {
+    return { isServer };
+  } else {
+    try {
+      return { isServer };
+    } catch (e) {
+      return { isServer };
+    }
+  }
+};
+
+export default Speakers;
