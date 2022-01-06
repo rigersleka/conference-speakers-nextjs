00dac7015bbab82f74193f7d69af0f5c25154bfb
Rigers Leka <rigers.leka@sisal.al>
2022-01-06 14:22:51 +0100

    used custom Container cmp into index

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,30 @@
 import App from '../src/App.js';
-import styles from '../styles/Home.module.css';
+import Container from '../src/Container.js';
 
 function Index({ isServer }) {
- 
-  console.log(isServer);
   return (
-    <div className={styles.container}>
+    <Container>
       <App pageName='Home' />
-    </div>
+    </Container>
   );
 }
 
+/* 
+If getServerSideProps or getInitialProps is used in a page, 
+Next.js will switch to render the page on-demand, per-request (meaning Server-Side Rendering).
+
+If the above is not the case, Nex.js will statically optimize your page 
+automatically by prerendering the page to static HTML.
+*/
 Index.getInitialProps = async ({ req }) => {
   const isServer = !!req;
   if (isServer) {
     return { isServer };
+  }
+  try {
+    return { isServer };
+  } catch (e) {
+    console.log(e);
   }
 };
 
