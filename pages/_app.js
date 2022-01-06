d09c67abaa064bb4edaef933a5ba011c2c454254
Rigers Leka <rigers.leka@sisal.al>
2022-01-06 14:21:57 +0100

    explanation of creation _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import '../styles/globals.css';
 
+/* ref: https://nextjs.org/docs/advanced-features/custom-app
+    Next.js uses App component to initialize pages. 
+    You can override it and control the page initialization. 
+*/
+
 const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
 
 export default MyApp;
