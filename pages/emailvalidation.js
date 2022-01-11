c413a12edc1dee35bba19f6c2f2a24d0f46d6f12
Rigers Leka <rigers.leka@sisal.al>
2022-01-11 11:50:31 +0100

    call emailValidation as _pages

---
+++ pages/emailvalidation.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import EmailValidationForm from '../src/EmailValidationForm.js'
+const emailValidation = () => {
+    return <EmailValidationForm />
+}
+
+export default emailValidation;
