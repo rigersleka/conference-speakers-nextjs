08b08457498e1955c4d4b5d0b3676e582bdf359d
Rigers Leka <rigers.leka@sisal.al>
2022-01-19 17:16:26 +0100

    UI implementation

--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,31 @@ import SignMeUp from './SignMeUp';
 import Navigation from './Navigation';
 
 const Home = () => {
-    return (<>
-        <div>
-           <SignMeUp/>
+  const signupCallback = (email) => {
+    console.log(`sign up called with email ${email}`);
+  };
+
+  return (
+    <>
+      <div className='jumbotron jumbotronheight'>
+        <div className='row'>
+          <div className='col-12 col-sm-4 text-center'>
+            <h6 className='text-uppercase'>October 19-20&nbsp;&nbsp;2019</h6>
+            <h6 className='text-uppercase'>San Jose, California</h6>
+          </div>
+          <div className='col-12 col-sm-8 text-lg-right'>
+            <h2>Silicon Valley Code Camp 2019</h2>
+            <div className='row col-12 text-lg-right'>
+              <SignMeUp signupCallback={signupCallback} />
+            </div>
+          </div>
         </div>
-        <div>
-            <Navigation/>
-        </div>
-    </>);
+      </div>
+      <div>
+        <Navigation />
+      </div>
+    </>
+  );
 };
 
 export default Home;
--- src/Navigation.js
+++ src/Navigation.js
@@ -2,18 +2,18 @@ import React from 'react';
 import Link from './Links';
 
 const Navigation = () => {
-    return (
-        <>
-            <nav className="navbar navbar-default">
-                <div className="container-fluid">
-                    <ul className="nav navbar-nav">
-                        <Link href='/'>Home</Link>
-                        <Link href='/speakers'>Speakers</Link>
-                    </ul>
-                </div>
-            </nav>
-        </>
-    );
+  return (
+    <nav className='navbar navbar-expand-sm bg-dark navbar-dark'>
+      <div className='navbar-nav'>
+        <li className='nav-item'>
+          <Link href='/'>Home</Link>
+        </li>
+        <li className='nav-item'>
+          <Link href='/speakers'>Speakers</Link>
+        </li>
+      </div>
+    </nav>
+  );
 };
 
 export default Navigation;
