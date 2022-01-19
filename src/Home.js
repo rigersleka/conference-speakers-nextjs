cc12d930fe1ded56d80906a4a3fe641efcf084b2
Rigers Leka <rigers.leka@sisal.al>
2022-01-19 16:34:23 +0100

    fix

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Login from './Login';
+import SignMeUp from './SignMeUp';
 import Navigation from './Navigation';
 
 const Home = () => {
     return (<>
         <div>
-            <Login/>
+           <SignMeUp/>
         </div>
         <div>
             <Navigation/>
